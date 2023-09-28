bb4c1f294fba9de6488f2a10a03057aab87a2037
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2023-09-28 09:19:17 -0700

    Stability updates

--- js/caltemplate.js
+++ js/caltemplate.js
@@ -22,15 +22,16 @@ class CalTemplate
     
         // Create the outer div
         const outerDiv = document.createElement('div');
-        outerDiv.classList.add('cl-restrictor');
-        // outerDiv.setAttribute('uk-grid', '');
+        outerDiv.classList.add('evt-restrictor','uk-text-center','uk-background-muted');
+
         outerDiv.style.backgroundColor = color;
         outerDiv.style.boxShadow = 'inset 1px 1px 100px #ffffff6b';
-        outerDiv.setAttribute('data-restrictor', `${month} ${year}`);
+        outerDiv.setAttribute('data-restrictor', `${month}-${year}`);
+
     
         // Create the card element
         const cardDiv = document.createElement('div');
-        cardDiv.classList.add('cl-restr-card');
+        cardDiv.classList.add('uk-text-bold');
         cardDiv.textContent = `${month} ${year}`;
     
         outerDiv.appendChild(cardDiv);
@@ -43,18 +44,46 @@ class CalTemplate
         let row = document.createElement('div');
         row.id = 'row_' + date;
         row.classList.add('cl-row');
-        row.setAttribute('cl-date', date);
+        row.setAttribute('data-date', date);
+        row.classList.add('uk-text-center','uk-grid-collapse','start-collapse','uk-background-muted','event-section','uk-grid');
 
-        let todateId = DateUtils.isDateToday(date) ? "id='row_today'" : "";
+        let day = CalTemplate.getDayOfWeek(date);
+        day = day.slice(0, 3);
+        let dnum = new Date(date).getUTCDate();
+
+        let todateId = DateUtils.isDateToday(date) ? "row_today" : "";
         let headdiv = document.createElement('div');
         headdiv.id = todateId;
-        headdiv.innerHTML = date;
-        headdiv.classList.add('cl-row-head');
+        headdiv.classList.add('uk-width-auto@m','uk-text-left','th-padding-small','uk-first-column');
+
+        let dnub = document.createElement('div');
+        dnub.id = '';
+        dnub.classList.add('uk-text-lead');
+        dnub.innerHTML = `${day} '${dnum}`;
+        
+        let dnub2 = document.createElement('div');
+        dnub2.id = '';
+        dnub2.classList.add('uk-card','uk-text-small');
+        dnub2.innerHTML = `${date}`;
+
+        headdiv.appendChild(dnub);
+        headdiv.appendChild(dnub2);
 
         let bodydiv = document.createElement('div');
         bodydiv.id = '';
-        bodydiv.classList.add('cl-row-body');
+        bodydiv.classList.add('uk-child-width-1-4@xl','uk-child-width-1-3@l','uk-child-width-1-2@m','uk-child-width-1-2@s','uk-grid-small','uk-grid-match','start-collapse','section-padding','uk-grid','uk-grid-stack','eventor-row-content');
 
+        for (let index = 0; index < items.length; index++) {
+          const element = items[index];
+          bodydiv.appendChild(element);
+        }
+        if (items.length == 0){
+          row.classList.add('eventor-hiddenrow');
+        }
+
+        if (todateId != ''){
+          row.classList.add('eventor-today');
+        }
         row.appendChild(headdiv);
         row.appendChild(bodydiv);
         
@@ -93,6 +122,7 @@ class CalTemplate
         dateIn.setAttribute('type', 'month');
         dateIn.setAttribute('name', 'flashdate');
         dateIn.setAttribute('value', curdate);
+        dateIn.style.width = '22px';
         calbutton.appendChild(dateIn);
 
         let nextbutton = document.createElement('div');
@@ -107,11 +137,11 @@ class CalTemplate
         nextexbutton.classList.add('cl-nav-button');
         nextexbutton.innerHTML = ">>";
 
-        div.appendChild(prevexbutton);
         div.appendChild(prevbutton);
+        div.appendChild(prevexbutton);
         div.appendChild(calbutton);
-        div.appendChild(nextbutton);
         div.appendChild(nextexbutton);
+        div.appendChild(nextbutton);
 
         return div;
       }
